aaf2a5b377e52aecd877a9e34edd93d88f99c02e
jatkrishan <krishanchoudhary13@gmail.com>
2023-08-02 09:30:47 +0530

    updateURL

--- config/configUrl.js
+++ config/configUrl.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME: "NOTIFACTION_DB" ,
-    DB_URL: "mongodb://127.0.0.1/notifaction_db"
+    DB_URL: "mongodb+srv://krishanchoudhary43:8432033943@crm-db.tfx8qqw.mongodb.net/"
 }
